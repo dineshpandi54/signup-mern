3a9370f0dd1961f3c09de351cc556b0564f04ea4
dineshpandi54 <dineshmanoher54@gmail.com>
2025-03-07 11:02:50 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app =express();
 app.use(bodyParser.json());
 app.use(cors(
     {
-        origin:["https://signup-mern-signup.vercel.app/"],
+        origin:["https://signup-mern-signup.vercel.app"],
         methods:["POST"],
         credentials:[true]
     }
